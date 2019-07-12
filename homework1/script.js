601967b0e34ada1f931edcfb2b715c5c25eae6a5
Begzat <begzat.kamen@gmail.com>
2019-07-12 16:42:35 +0600

    first parts final

--- homework1/script.js
+++ homework1/script.js
@@ -16,7 +16,11 @@ for (let i = 0; i < 2; i++) {
 
     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
         a != '' && b != '' && a.length < 50) {
+        console.log("done");
         appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
     }
 };
 
